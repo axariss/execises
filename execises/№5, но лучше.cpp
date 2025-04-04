7a7a2b81d20da55a1fd06d61acb41b1cf548cd22
Арина Сарсембаева <arisha.sarsem@gmail.com>
2025-04-04 21:00:18 +0300

    -

---
+++ execises/№5, но лучше.cpp
@@ -0,0 +1,21 @@
+/*На просторах литкод заинтересовалась и другим подходом к данной задаче. 
+Увидев идею, решила реализовать её самостоятельно. Данная реализация сокращает 
+использование памяти и кажется мне более элегантной :).*/
+
+
+#include <iostream>
+#include <limits>
+using namespace std;
+
+
+int reverse(int x) {
+	int x_reversed = 0;
+	while (x != 0) {
+		if (x_reversed > INT_MAX / 10 || x_reversed < INT_MIN / 10)
+			return 0;
+		x_reversed = x_reversed * 10 + x % 10;
+		x = x / 10;
+	}
+	return x_reversed;
+}
+
